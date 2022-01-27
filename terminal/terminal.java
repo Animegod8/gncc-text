a980e4c64dfd0ba9b76434b1efb1ec0315565a81
Animegod8 <60558787+Animegod8@users.noreply.github.com>
2022-01-27 08:20:06 -0700

    Terminal console

---
+++ terminal/terminal.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class terminal{
+  public static void main(String[] args){
+    System.out.println("Welcome to the terminal");
+
+    terminal.newLine();
+  }
+
+  public static void newLine(){
+  Scanner myObj = new Scanner(System.in);
+
+  String command = myObj.nextLine();
+
+  switch(command){
+    case "test":
+      System.out.println("Hello world!");
+      terminal.newLine();
+    break;
+    case "writeFile":
+      System.out.println("Use the file write class");
+    case "exit":
+      System.out.println("Ending session, run the class file again to use the terminal again");
+    break;
+    default:
+    System.out.println("COMMAND NOT FOUND");
+    terminal.newLine();
+  }
+}
+}
