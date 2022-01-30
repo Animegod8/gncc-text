42934d1b8b9c73333dddbff698bfe0846e184b7f
Animegod8 <60558787+Animegod8@users.noreply.github.com>
2022-01-29 17:25:04 -0700

    The main editor

--- editor.py
+++ editor.py
@@ -4,19 +4,20 @@ def newLine():
   if text == '.exit':
     print('goodbye for now!')
   elif text == '.exe':
-    print('run the shell file file for the terminal to execute your code(note not added yet)')
+    print('run the shell file file for the terminal to execute your code(note affter running the shell script once then run the terminal.class file every time affter)')
+    print('also copy and paset your code to write it to a file in the compile.class')
   else:
     newLine()
 
-#
 
 # code to inisialize on startup bla bla bla
 inText = 0
 print('chose witch mode to boot into(editor(miniTerm comming later)')
-print('NOTICE: IN PRE RELESE NOTHING IS FINAL')
+print('NOTICE: ITS A TERMINAL TECH DEMO')
 bootMode = input()
 if bootMode == 'editor':
   print('welcome to the editor!')
   print('just type some lines and your writing some code!')
   inText = 1
   newLine()
+
